a61c7af235a38f919af8a89b1cb32696833c91db
Patrick Zawadzki <zawadzkip@msoe.edu>
2015-05-11 21:06:17 -0500

    Errors fixed, current project. Started implementing ip information

--- IP Locator/IP Locator/IPRequest.swift
+++ IP Locator/IP Locator/IPRequest.swift
@@ -8,10 +8,43 @@
 
 import Foundation
 import Alamofire
+import SwiftyJSON
 
 class IPRequest : NSObject{
+    private let baseURL = "ipinfo.io/"
+    private let json = "json"
+    private var ipQueried:String = ""
+    private var city:String = ""
+    private var region:String = ""
+    private var latitude:String = ""
+    private var longitude:String = ""
+    private var country:String = ""
+    private var postalCode:String  = ""
     
-    func init(){
-        
+    override init(){
+        //does't really do much
     }
+    
+    func sendMyIPInfo() -> Void{
+        Alamofire.request(.GET, "http://ipinfo.io/json").responseJSON{ (_,_,json,error) in
+            if(error == nil){
+                var json = JSON(json!)
+                var geoArray = json["loc"].string!.componentsSeparatedByString(",")
+                self.ipQueried = json["ip"].string!
+                self.city = json["city"].string!
+                self.region = json["region"].string!
+                self.country = json["country"].string!
+                self.latitude = geoArray[0]
+                self.longitude = geoArray[1]
+                self.postalCode = json["postal"].string!
+            }else{
+                var alert = UIAlertView(title: "Request Error", message: "There was an error with the Response", delegate: nil, cancelButtonTitle: "Ok")
+                alert.show()
+            }
+            
+        }
+    }
+    
+    
+    
 }
---
+++ IP Locator/Pods/Target Support Files/Pods-IP Locator-SwiftyJSON/Pods-IP Locator-SwiftyJSON-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_IP_Locator_SwiftyJSON : NSObject
+@end
+@implementation PodsDummy_Pods_IP_Locator_SwiftyJSON
+@end
