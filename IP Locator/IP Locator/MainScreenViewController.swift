fb0b83f09173f1ec14919e50eb11a1337f01e6ea
Patrick Zawadzki <zawadzkip@msoe.edu>
2015-05-14 07:54:56 -0500

    Added animation view, started creating home screen.

---
+++ IP Locator/IP Locator/MainScreenViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MainScreenViewController.swift
+//  IP Locator
+//
+//  Created by Patrick Zawadzki on 5/13/15.
+//  Copyright (c) 2015 PatZawadzki. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MainScreenViewController : UIViewController{
+    
+    
+    override func viewDidLoad() {
+        //After all elements are added, have each individual part
+        //fade in, change alpha value from 0 to 1.
+        
+    }
+    
+    
+    override func viewDidAppear(animated: Bool) {
+        
+    }
+}
--- IP Locator/IP Locator/ViewController.swift
+++ IP Locator/IP Locator/ViewController.swift
@@ -7,18 +7,44 @@
 //
 
 import UIKit
+import Foundation
 
-class ViewController: UIViewController {
+class LoadingViewController: UIViewController {
 
+    
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        let IP = IPRequest()
-        IP.sendMyIPInfo()
+        imageView.userInteractionEnabled = false
+        self.load()
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if var destination = segue.destinationViewController as? MainScreenViewController{
+            
+        }
+    }
+    
+    private func load(){
+        imageView.animationImages = [UIImage]()
+        imageView.animationRepeatCount = 1
+        for var index = 1; index < 36; index += 1{
+            var frameName = String(format: "%d", index)
+            imageView.animationImages?.append(UIImage(named: frameName)!)
+        }
+        imageView.animationDuration = 1
+        imageView.startAnimating()
+        var triggerTime = (Int64(NSEC_PER_SEC) * 1)
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, triggerTime), dispatch_get_main_queue(), { () -> Void in
+            self.performSegueWithIdentifier("finishedAnimatingSegue", sender: self)
+        })
+        
     }
 
 
